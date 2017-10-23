eeb665fa5afe08144690cb07fdd5fe5cd4145c7b
wafa yahyaoui <hoffa.yahyaoui@gmail.com>
2017-10-23 19:59:25 +0100

    reset password working

--- peculium_project/apps/accounts/urls.py
+++ peculium_project/apps/accounts/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+
+from django.contrib.auth import views as auth_views
 from . import views
-from .views import IndexView, LoginUserView, UpdateUserView, LogoutUserView, PayementView, account_activation_sent, activate, signup, UpdateAdminView
+from .views import IndexView, LoginUserView, UpdateUserView, LogoutUserView, PayementView, account_activation_sent, \
+    activate, signup, UpdateAdminView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index_view'),
@@ -18,4 +21,12 @@ urlpatterns = [
     # activate with token
     url(r'activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
+    # url(r'^user/', include('django.contrib.auth.urls')),
+
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+
 ]
